ec633b6a5367bcb59e433cf9611809bd4f543906
Daniel Fernandez <mdanif9728@gmail.com>
2025-06-26 16:53:29 -0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/NavBar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function NavBar() {
+  return (
+    <>
+      <section></section>
+    </>
+  );
+}
