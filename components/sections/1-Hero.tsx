9b7e0785487c47e5781af928ee6190f159a31caa
Daniel Fernandez <mdanif9728@gmail.com>
2025-06-26 20:36:01 -0700

    Prepare the Hero-section base content.

--- components/sections/1-Hero.tsx
+++ components/sections/1-Hero.tsx
@@ -1,23 +1,37 @@
 "use client";
 
 import Image from "next/image";
+
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
     <>
-      <main>
-        <section>
-          <h1></h1>
-          <p></p>
-        </section>
+      <section className="flex flex-col justify-center items-center w-full h-full m-auto">
+        <div>
+          <h1>Breathe Better with LEVOIT</h1>
+          <p>Transform your bedroom into a clean-air sanctuary.</p>
+        </div>
 
-        <section>
+        <div className="flex flex-col items-center justify-center m-auto">
           <AspectRatio ratio={16 / 9}>
             <Image src="..." alt="Image" className="rounded-md object-cover" />
           </AspectRatio>
-        </section>
-      </main>
+          <p className="text-[#077D61]">In Stock</p>
+        </div>
+
+        <div>
+          <Button variant="outline">Button</Button>
+        </div>
+
+        <div>
+          <ul className="...">
+            <li>Free Return</li>
+            <li>Free Delivery</li>
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
