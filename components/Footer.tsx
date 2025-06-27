f2684f592a1058bb5be3aa154c45e15ee635266c
Daniel Fernandez <mdanif9728@gmail.com>
2025-06-26 19:42:39 -0700

    Finish with the footer styling.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,18 @@
 export default function Footer() {
   return (
     <>
-      <section></section>
+      <section className="flex flex-col absolute bottom-0 justify-center p-8 gap-8 bg-[#222F3E]">
+        <div className="flex flex-col gap-4 text-center text-white">
+          <h1 className="text-2xl font-bold">CleanAir.com</h1>
+          <p>
+            This site is not affiliated with or endorsed by LEVOIT or its parent
+            companies.
+          </p>
+          <div className="bg-[#364558] rounded-full py-2 px-4 text-center">
+            <p>All Right Reserved - 2025</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
