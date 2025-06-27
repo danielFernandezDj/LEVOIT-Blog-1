a10659b2fbd7a69e17650e331bd0b296fcb1aa06
Daniel Fernandez <mdanif9728@gmail.com>
2025-06-26 20:23:58 -0700

    Instaling aspect-ratio dependencie.

--- components/sections/1-Hero.tsx
+++ components/sections/1-Hero.tsx
@@ -1,10 +1,22 @@
 "use client";
 
+import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
 export default function Hero() {
   return (
     <>
       <main>
-        
+        <section>
+          <h1></h1>
+          <p></p>
+        </section>
+
+        <section>
+          <AspectRatio ratio={16 / 9}>
+            <Image src="..." alt="Image" className="rounded-md object-cover" />
+          </AspectRatio>
+        </section>
       </main>
     </>
   );
