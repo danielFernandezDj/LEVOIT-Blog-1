96a627be7f474d3abb54e9bf586a6ac87437263e
Daniel Fernandez <mdanif9728@gmail.com>
2025-06-26 17:15:59 -0700

    Preparing a INFO.md file with some app-expecifications

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,6 @@ export default function Home() {
       </Head>
 
       {/* Components */}
-
-      <Footer />
     </div>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,13 @@
 export default function NavBar() {
   return (
     <>
-      <section></section>
+      <section className="flex justify-evenly items-center ">
+        <h1>CleanAir.com</h1>
+        <div className="">
+            <a href="" className="text-[#]">Buy Now</a>
+            <a href=""></a>
+        </div>
+      </section>
     </>
   );
 }
